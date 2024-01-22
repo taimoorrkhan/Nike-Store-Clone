5bd794711165f12624cb484a33766af518e0b336
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2024-01-22 21:48:06 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet,  View,  } from 'react-native';
+import ProductScreen from './src/screen/productScreen';
+import ProductDetailsScreen from './src/screen/productDetailsScreen';
 export default function App() {
   
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/*  <ProductScreen /> */}
+      <ProductDetailsScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +19,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  inputContainer: {
+    width: "50%",
+    padding: 1,
   },
 });
